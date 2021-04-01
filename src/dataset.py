55951e61eb0b033a6a98fb7812880e1d103c5637
livioschlaepfer <livios@student.ethz.ch>
2021-04-01 10:00:04 +0200

    remove dataset limiter

--- src/dataset.py
+++ src/dataset.py
@@ -18,7 +18,7 @@ def init_train_dataloaders(config):
     print('Creating training and validation splits')
 
     # Create training and validation splits
-    image_paths_train, image_paths_val, mask_paths_train, mask_paths_val = train_test_split(image_paths[:3], mask_paths[:3], test_size = config.val_size, random_state = config.seed)
+    image_paths_train, image_paths_val, mask_paths_train, mask_paths_val = train_test_split(image_paths, mask_paths, test_size = config.val_size, random_state = config.seed)
     
     image_paths = {'train': image_paths_train, 'val': image_paths_val}
     mask_paths = {'train': mask_paths_train, 'val': mask_paths_val}
