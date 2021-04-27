7a0c8c2de04265c3365cb0274db106c68a1874bf
livioschlaepfer <livios@student.ethz.ch>
2021-04-27 10:16:08 +0200

    update paths sven

--- src/paths.py
+++ src/paths.py
@@ -1,8 +1,12 @@
 def paths_setter(username):
         if username == "svenk":
             path_dict = {
-                    'train_mask_dir': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/training/groundtruth/1',
-                    'train_image_dir': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/training/images/1',
+                    'train_mask_dir': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/training/groundtruth/1_aug',
+                    'train_mask_dir_aug_input': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/training/groundtruth/1',
+                    'train_mask_dir_aug_output': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/training/groundtruth/1_aug',
+                    'train_image_dir': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/training/images/1_aug',
+                    'train_image_dir_aug_input': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/training/images/1',
+                    'train_image_dir_aug_output': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/training/images/1_aug',
                     'test_image_dir': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/test_images/test_images',
                     'test_output_dir': 'C:/Users/svenk/OneDrive/Desktop/ETH_SS_21/Computational_Intelligence_Lab/Project/Data/test_images',
             }
