1ef8465c7ddfaa62dbc0c0028394431238384927
livioschlaepfer <livios@student.ethz.ch>
2021-06-24 15:49:01 +0200

    calculate loss on underrepresented foreground class

--- src/criterion/cldice_loss.py
+++ src/criterion/cldice_loss.py
@@ -3,9 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# TODO: as mentioned in paper compute loss only over underrepresented class
-
-
 def soft_erode(img):
 
     # print("soft erode shape", img.shape)
@@ -55,8 +52,8 @@ class soft_cldice(nn.Module):
     def forward(self, y_true, y_pred):
         skel_pred = soft_skel(y_pred, self.iter)
         skel_true = soft_skel(y_true, self.iter)
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true)) + self.smooth) / (torch.sum(skel_pred) + self.smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred)) + self.smooth) / (torch.sum(skel_true) + self.smooth)    
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,0]) + self.smooth) / (torch.sum(skel_pred[:,0]) + self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,0]) + self.smooth) / (torch.sum(skel_true[:,0]) + self.smooth)    
         cl_dice = 1.- 2.0 * (tprec * tsens) / (tprec + tsens)
         
         return cl_dice
@@ -65,8 +62,8 @@ class soft_cldice(nn.Module):
 def soft_dice(y_true, y_pred):
     smooth = 1
 
-    intersection = torch.sum((y_true * y_pred))
-    coeff = (2. *  intersection + smooth) / (torch.sum(y_true) + torch.sum(y_pred) + smooth)
+    intersection = torch.sum((y_true * y_pred)[:,0])
+    coeff = (2. *  intersection + smooth) / (torch.sum(y_true[:,0]) + torch.sum(y_pred[:,0]) + smooth)
     return (1. - coeff)
 
 
@@ -81,8 +78,8 @@ class soft_dice_cldice(nn.Module):
         dice = soft_dice(y_true, y_pred)
         skel_pred = soft_skel(y_pred, self.iter)
         skel_true = soft_skel(y_true, self.iter)
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true)) + self.smooth) / (torch.sum(skel_pred) + self.smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred)) + self.smooth) / (torch.sum(skel_true) + self.smooth)    
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,0]) + self.smooth) / (torch.sum(skel_pred[:,0]) + self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,0]) + self.smooth) / (torch.sum(skel_true[:,0]) + self.smooth)    
         cl_dice = 1. - 2.0 * (tprec * tsens) / (tprec + tsens)
         
         return (1.0 - self.alpha) * dice + self.alpha * cl_dice
