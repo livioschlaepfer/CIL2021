b7e858071582ef5e29bdf3c63a9ea54244ad8519
livioschlaepfer <livios@student.ethz.ch>
2021-06-25 14:25:43 +0200

    fix focal loss

--- src/criterion/focal_loss.py
+++ src/criterion/focal_loss.py
@@ -10,16 +10,29 @@ class FocalLoss(nn.Module):
 
     """
 
-    def __init__(self, alpha=40, gamma=2, smooth=1e-5):
+    def __init__(self, alpha=0.1, gamma=2, smooth=1e-5):
         super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
         self.smooth = smooth
 
     def forward(self, input, target):
-        p_1 = input[target == 1]
-        p_0 = (1. - input)[target == 0]
-        p_t = p_0 + p_1
-        focal = - self.alpha * (1 - p_t) ** self.gamma * torch.log(p_t)
 
-        return torch.mean(focal)     
+        input = input + self.smooth
+        
+        p_1 = input * target
+        p_0 = (1. - input)[target == 1] = 0
+        p_t = p_1 + p_0
+
+        p_t = torch.clamp(p_t, self.smooth, 1. - self.smooth)
+
+        focal_1 = - self.alpha * torch.pow((1 - p_t), self.gamma) * torch.log(p_t)
+        focal_0 = - (1. - self.alpha) * torch.pow((1 - p_t), self.gamma) * torch.log(p_t)
+
+        focal_1[target == 0] = 0
+        focal_0[target == 1] = 0
+
+        focal_1 = torch.clamp(focal_1, self.smooth, 1. - self.smooth)
+        focal_1 = torch.clamp(focal_0, self.smooth, 1. - self.smooth)
+
+        return torch.sum(focal_1 + focal_0)     
