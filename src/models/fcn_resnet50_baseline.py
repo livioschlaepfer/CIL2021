af44073bd11fd432f9f57047cb7b69125ff4c2ec
svkohler <svenkohler1995@hotmail.com>
2021-06-26 17:29:10 +0200

    incorporated matse baselines

---
+++ src/models/fcn_resnet50_baseline.py
@@ -0,0 +1,97 @@
+from PIL import Image
+import os
+import torch
+import torch.nn as nn
+from torchvision import models
+from torchvision.models.segmentation.fcn import FCNHead
+from torchvision import transforms
+
+
+from src.criterion.dice_loss import dice_loss
+
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+class FCNResnet50RunnerClass:
+    def __init__(self, config):
+        self.config = config
+
+        self.init_model()
+        self.init_criterion()
+
+    def init_model(self):
+        print('Initializing model')
+
+        model = models.segmentation.fcn_resnet50(pretrained=True, progress=True, aux_loss=None)  # Mathias: adjusted model here compared to Deeplabrunner
+        
+        # Update number of segmentation classes in classifier and auxillary classifier
+        model.classifier = FCNHead(2048, self.config.num_classes) # Mathias: adjusted to FCNHead here compared to Deeplabrunner
+        model.aux_classifier = FCNHead(1024, self.config.num_classes)
+        
+        # Print the model we just instantiated
+        print(model)
+
+        self.model = model
+
+    def init_criterion(self):
+        # Setup the loss
+        # self.criterion = torch.nn.MSELoss(reduction='mean')
+        # self.criterion = nn.CrossEntropyLoss()
+        # self.criterion = nn.BCELoss()
+        # self.criterion = nn.NLLLoss()
+        # self.criterion = DiceLoss()
+
+        def forward(input, target):
+
+            bce = nn.BCELoss()
+            dice = dice_loss()
+
+            loss1 = 0.0 * bce(input, target) + 0.8 * dice(input, target)
+
+            input_16 = self.image_to_patched(input, 16)
+            target_16 = self.mask_to_patched(target, 16)
+
+            loss3 = 0.0 * bce(input_16, target_16) + 0.8 * dice(input_16, target_16)
+
+            # print("loss1", loss1,"loss3", loss3, )
+
+            return loss1 + 0.2 * loss3 
+
+        self.criterion = forward
+  
+    def forward(self, inputs):
+        outputs = self.model(inputs)['out']
+        outputs = torch.sigmoid(outputs)
+
+        return outputs
+
+    def loss(self, outputs, labels):
+        loss = self.criterion(outputs.float(), labels.float())
+        return loss
+
+    def convert_to_png(self, output):
+        binary = output.argmin(0)
+        binary = torch.tensor(binary, dtype=torch.float64)
+        binary = transforms.ToPILImage(mode="L")(binary).convert("RGB")
+
+        return binary
+
+    # assign a label to a patch
+    def mask_to_patched(self, mask, patch_size = 16):
+        foreground_threshold = 0.25
+        patcher = nn.AvgPool2d(patch_size)
+
+        mask = patcher(mask)
+
+        thresholded_mask = mask
+        thresholded_mask[:,0][mask[:,0] > foreground_threshold] = 1
+        thresholded_mask[:,1][mask[:,1] <= foreground_threshold] = 0
+        
+        return thresholded_mask
+
+
+    def image_to_patched(self, image, patch_size):
+        patcher = nn.AvgPool2d(patch_size)
+
+        return patcher(image)
--- src/models/model_runner.py
+++ src/models/model_runner.py
@@ -1,9 +1,23 @@
-
-
 from src.models.deeplabv3 import DeepLabv3RunnerClass
+from src.models.deeplabv3_regularizer import DeepLabv3RegularizerRunnerClass
+from models.trivial_baseline import TrivialRunnerClass
+from models.fcn_resnet50_baseline import FCNResnet50RunnerClass
+from models.unet_baseline import UNetRunnerClass
 
 
 def init_runner(config):
     if config.model_name == "deeplabv3":
         runner = DeepLabv3RunnerClass(config)
+        return runner
+
+    if config.model_name == "trivial_baseline":
+        runner = TrivialRunnerClass(config)
+        return runner
+
+    if config.model_name == "fcnres50_baseline": #pretrained
+        runner = FCNResnet50RunnerClass(config)
+        return runner
+    
+    if config.model_name == "unet_baseline": #not pretrained
+        runner = UNetRunnerClass(config)
         return runner
